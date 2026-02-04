17b46f6015e82b985afab64d8d10468e574ec289
codermohit0718 <codermohit0718@gmail.com>
2026-02-04 10:06:01 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x;
+    x = 5;
+    int y;
+    y = 10;
+    cout<<x+y;
+    cout<<endl;
+    cout<<x-y;
+    cout<<endl;
+     cout<<x*y;
+    cout<<endl;
+     cout<<x/y;
+    
+
+}
