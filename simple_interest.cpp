1bb0a656e9e4d4669e24c55ae08eaf8d41816b52
codermohit0718 <codermohit0718@gmail.com>
2026-02-05 09:52:54 +0530

    Add files via upload

---
+++ simple_interest.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int simple_interest;
+    int p = 10;
+    int r = 20;
+    int t = 30;
+    simple_interest = (p*r*t)/100;
+    cout<<simple_interest;
+}
