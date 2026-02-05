6524899e84c6f184c593c5520b7aa8d7ee01edef
codermohit0718 <codermohit0718@gmail.com>
2026-02-05 14:43:14 +0530

    Add files via upload

---
+++ check_even_odd_conditional.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x;
+    cout<<"enter the value of x:";
+    cin>>x;
+    if(x%2==0){
+        cout<<"x is even number";
+    }
+    else{
+        cout<<"x is odd number";
+    
+    }
+
+}
